e125647c1da273b0b50e153d9d194934bf5e02d6
Debanjan Sengupta <bubai_babi2013@live.in>
2014-02-27 02:17:54 +0530

    PHASE2#All error handled from application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,21 +9,37 @@ class ApplicationController < ActionController::Base
   @primary_languages = Language.first
 
   before_filter :set_locale
-  
-  def set_locale 
+
+
+  begin
+    # do something dodgy
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = "The resource is unavailable"
+  rescue ActiveRecord::ActiveRecordError
+    flash[:notice] = "Active Record Error occured. Please contact administrator."
+  rescue # StandardError
+    flash[:notice] = "Active Record Error occured. Please contact administrator."
+  rescue Exception
+    flash[:notice] = "Sorry an error occured. Please contact administrator."
+  ensure
+    redirect_to root_path and return
+  end
+
+
+  def set_locale
     if signed_in?
       if session[:locale].present?
-        
+
       else
         session[:locale] = current_user.locale
       end
     end
-    
-    session[:locale] = "en" unless session[:locale].present? 
+
+    session[:locale] = "en" unless session[:locale].present?
     I18n.locale = session[:locale]
 
   end
-  
+
   def change_locale(l)
     session[:locale] = l
     set_locale
@@ -32,11 +48,10 @@ class ApplicationController < ActionController::Base
   def default_url_options(options = {})
     {:locale => I18n.locale}
   end
-  
-  
-  
+
+
   def is_number?(object)
     true if Float(object) rescue false
-  end  
+  end
 
 end
