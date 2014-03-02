3d98ad3720c215bf6c334dc130126c87d013a01e
Debanjan Sengupta <bubai_babi2013@live.in>
2014-03-03 00:31:55 +0530

    Production Mailer Settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Wpcc::Application.configure do
   config.action_controller.perform_caching = false
 
   # - care if the mailer can't send
-  #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,16 +17,14 @@ Wpcc::Application.configure do
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default :charset => "utf-8"
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #  port: 587,
-  #   domain: "wpcc.herokuapp.com",
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #  user_name: ENV["GMAIL_USERNAME"],
-  #  password: ENV["GMAIL_PASSWORD"]
-  #}
+  ActionMailer::Base.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :authentication => :plain,
+      :user_name            => 'dave4u90@gmail.com',
+      :password       => ' rklseraqmlynarzt',
+      :enable_starttls_auto => true
+  }
 
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
