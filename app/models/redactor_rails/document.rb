e675c033f8210600918a14363a5d31c2d0a0fdd9
Debanjan Sengupta <bubai_babi2013@live.in>
2014-03-29 12:20:52 +0530

    Carrierwave/ ActiveRecord orm included

--- app/models/redactor_rails/document.rb
+++ app/models/redactor_rails/document.rb
@@ -1,3 +1,4 @@
+require 'carrierwave/orm/activerecord'
 class RedactorRails::Document < RedactorRails::Asset
   mount_uploader :data, RedactorRailsDocumentUploader, :mount_on => :data_file_name
 
--- app/models/redactor_rails/picture.rb
+++ app/models/redactor_rails/picture.rb
@@ -1,3 +1,4 @@
+require 'carrierwave/orm/activerecord'
 class RedactorRails::Picture < RedactorRails::Asset
   mount_uploader :data, RedactorRailsPictureUploader, :mount_on => :data_file_name
 
